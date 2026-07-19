bc2a0ca695e090e3f5124431cffb970ce7d6b66b
AlinaWan <wuangelina40@gmail.com>
2026-07-18 23:28:52 -0700

    feat: skip calling vcvars64.bat if already in native tools command prompt

--- build_native.cmd
+++ build_native.cmd
@@ -1,9 +1,12 @@
 @echo off
+if /I "%VSCMD_ARG_TGT_ARCH%"=="x64" goto :vcvars64_found
+
 echo [96m######################################### Initializing environment ##########################################[0m
 for %%E in (Community Professional Enterprise BuildTools) do (
     for %%V in (18 2022) do (
         if exist "%ProgramFiles%\Microsoft Visual Studio\%%V\%%E\VC\Auxiliary\Build\vcvars64.bat" (
             call "%ProgramFiles%\Microsoft Visual Studio\%%V\%%E\VC\Auxiliary\Build\vcvars64.bat"
+            echo [92m########################################## Environment initialized ##########################################[0m
             goto :vcvars64_found
         )
     )
@@ -12,7 +15,6 @@ echo [31m!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Could not find vcvars64.bat !
 exit /b 1
 
 :vcvars64_found
-echo [92m########################################## Environment initialized ##########################################[0m
 echo [96m########################################### Starting engine build ###########################################[0m
 @echo on
 call build_engine_native.cmd
